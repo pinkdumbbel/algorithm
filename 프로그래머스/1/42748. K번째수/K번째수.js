605b8b6dc876ee398b315246f1a4e4c492606ac1
pinkdumbbel <tlacjd665@gmail.com>
2024-06-02 16:00:07 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+    var answer = [];
+    
+    commands.forEach(([i,j,k]) => {
+        answer.push(array.slice(i-1, j).sort((a,b) => a-b)[k-1])
+    })
+    
+    
+    return answer;
+}
