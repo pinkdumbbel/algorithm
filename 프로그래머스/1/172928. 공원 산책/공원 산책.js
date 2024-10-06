3dd27e07024affa1810394b7cf04f8a2ab8aa148
pinkdumbbel <tlacjd665@gmail.com>
2024-10-06 16:59:43 +0900

    [level 1] Title: 공원 산책, Time: 0.68 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -1,60 +1,23 @@
 function solution(park, routes) {
-    var answer = [];
-    let cp = [];
-    const obstacle = [];
-    const [maxH, maxW] = [park.length-1, park[0].length-1];
-    const directionMap = {
-        N: [-1, 0],
-        S: [1, 0],
-        W: [0, -1],
-        E: [0, 1],
-    }
-    
-    for(let i = 0; i < park.length; i++) {
-        for(let j = 0; j < park[i].length; j++) {
-            const s = park[i][j];
-            const position = [i,j];
-            
-            if(s === 'S') {
-                cp = position;
-            } else if(s === 'X') {
-              obstacle.push(position)  
-            };
-        };
-    };
-    
-    routes.forEach((r) => {
-       const [rd, rm] = [r.split(' ')[0], Number(r.split(' ')[1])];
-       const [h, w] = directionMap[rd];
-       const [moveH, moveW] = [h*rm, w*rm];
-       const [currentH, currentW] = cp;
-        
-       if(moveH !== 0) {
-           let ch = currentH;
-           
-           for(let i = 0; i !== moveH; i += h) {
-               ch += h;
-               if((ch < 0 || ch > maxH) || park[ch][currentW] === 'X') {
-                   ch = currentH;
-                   break;
-               }   
-           }
-           cp = [ch, currentW];
-       } else if(moveW !== 0) {
-         let cw = currentW;
-         const p = park[currentH];
-         for(let i = 0; i !== moveW; i += w) {
-             cw += w;
-             if((cw < 0 || cw > maxW) || p[cw] === 'X') {
-                   cw = currentW;
-                   break;
-             }
-         };
-           
-         cp = [currentH, cw];  
-       };
-        
-    });    
-    
-    return cp;
-}
+        const dirs = { E: [0, 1], W: [0, -1], S: [1, 0], N: [-1, 0] };
+        let [x, y] = [0, 0];
+        for (let i = 0; i < park.length; i++) {
+          if (park[i].includes('S')) {
+            [x, y] = [i, park[i].indexOf('S')];
+            break;
+          }
+        }
+
+        routes.forEach((route) => {
+          const [r, n] = route.split(' ');
+          let [nx, ny] = [x, y];
+          let cnt = 0;
+          while (cnt < n) {
+            [nx, ny] = [nx + dirs[r][0], ny + dirs[r][1]];
+            if (!park[nx] || !park[nx][ny] || park[nx][ny] === 'X') break;
+            cnt++;
+          }
+          if (cnt == n) [x, y] = [nx, ny];
+        });
+        return [x, y];
+      }
