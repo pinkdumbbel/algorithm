d4e03046cbdb1a86bfc8540b6b33a347058b1be4
pinkdumbbel <tlacjd665@gmail.com>
2024-10-13 16:22:43 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 32.74 ms, Memory: 39.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -0,0 +1,41 @@
+function solution(friends, gifts) {
+    let answer = 0;
+    const give = {};
+    const score = {};
+    
+    for(let i = 0; i < friends.length; i++) {
+       score[friends[i]] = 0;
+        
+       for(let j = 0; j < friends.length; j++) {
+           if(i === j) continue;
+           give[friends[i]] = {...give[friends[i]],[friends[j]]: 0};
+       };
+    };
+
+
+    gifts.forEach((gift) => {
+        const [giver, taker] = gift.split(' ');
+        give[giver][taker] = (give[giver][taker]??0)+1;
+        score[giver] += 1;
+        score[taker] -= 1;
+    });
+    
+    friends.forEach((giver) => {
+       let cnt = 0;
+        
+       for(const [taker, takeCnt] of Object.entries(give[giver])){
+           const giveCnt = (give[taker][giver]??0);
+           const diff = takeCnt - giveCnt;
+           
+           if(diff === 0) {               
+               cnt += (score[giver] > score[taker] ? 1 : 0);
+           } else {
+               cnt += (diff > 0 ? 1 : 0)
+           };
+       }
+
+       answer = Math.max(answer,cnt);
+    });
+    
+    return answer;
+}
