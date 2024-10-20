481ff8b3c8b8603a5c45f07e8a0d6ffb1b88ab0b
pinkdumbbel <tlacjd665@gmail.com>
2024-10-20 15:37:01 +0900

    [level 2] Title: 숫자의 표현, Time: 2.29 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,13 +1,16 @@
 function solution(n) {
-    let answer=0, lt=1, sum=0;
-    
-    for(let rt = 1; rt <= n; rt++) {
-        sum += rt;
-        while(sum > n) {
-          sum -= lt++
-        };
-        if(sum === n) answer++;        
-    };
-    
-    return answer;
+    let count = 0;
+    for (let start = 1; start <= n; start++) {
+        let sum = 0;
+        for (let num = start; num <= n; num++) {
+            sum += num;
+            if (sum === n) {
+                count++;
+                break;
+            } else if (sum > n) {
+                break;
+            }
+        }
+    }
+    return count;
 }
