69f45e4dd333ca9651c927ce935b9170e2553fa3
pinkdumbbel <tlacjd665@gmail.com>
2024-10-20 15:33:43 +0900

    [level 2] Title: 숫자의 표현, Time: 1.46 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let answer=0, lt=1, sum=0;
+    
+    for(let rt = 1; rt <= n; rt++) {
+        sum += rt;
+        while(sum > n) {
+          sum -= lt++
+        };
+        if(sum === n) answer++;        
+    };
+    
+    return answer;
+}
