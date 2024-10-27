23c39734452c05545ce2121c25e9334294fa7c87
pinkdumbbel <tlacjd665@gmail.com>
2024-10-27 17:34:12 +0900

    [level 2] Title: 짝지어 제거하기, Time: 57.56 ms, Memory: 50.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    const stack = []
+    
+    for(const x of s) {
+        if(stack[stack.length-1] === x) {
+            stack.pop();
+        } else {
+            stack.push(x);
+        }
+    };
+    
+    return stack.length === 0 ? 1 : 0
+}
