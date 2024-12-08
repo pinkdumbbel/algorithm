15d1dceadd45304ee181b5e6e3612753acedd07a
pinkdumbbel <tlacjd665@gmail.com>
2024-12-08 17:18:38 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 78.16 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=javascript) 
 
 ### 성능 요약
 
-메모리: 57.1 MB, 시간: 1112.11 ms
+메모리: 57 MB, 시간: 78.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 08일 16:43:32
+2024년 12월 08일 17:18:37
 
 ### 문제 설명
 
--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,27 +1,12 @@
-function solution(elements) {    
-    let answer = 0;
+function solution(elements) {
+    const circular = elements.concat(elements);
     const set = new Set();
-    const len = elements.length;
-    
-    for(let i = 0; i < len; i++) {
-        for(let j = 0; j < len; j++) {
-            let sum = 0;
-            for(let k = j; k <= i+j; k++) {
-                const idx = k >= len ? k-len : k;
-                sum += elements[idx];  
-            };
+    for (let i = 0; i < elements.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < elements.length; j++) {
+            sum += circular[i + j];
             set.add(sum);
         }
-    };
-    
+    }
     return set.size;
 }
-
-//중복 제거를 위해 Set을 선언
-//elements를 순회 하면서 루프 한번 돌 때 마다 elements를 3중 반복으로 실행
-//i = 0부터 elements.length-1 까지 순회
-//j = 0부터 elements.length-1 까지 순회
-//k = j부터 i+j 까지 순회
-//k가 순회 하면서 값을 누적하고 루프가 종료되면 Set에 저장 한다.
-//Set의 사이를 answer에 누적한다.
-
