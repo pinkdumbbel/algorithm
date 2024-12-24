91eaf3a8dd03b010b1edcb19754f1898152fb668
pinkdumbbel <tlacjd665@gmail.com>
2024-12-24 19:03:58 +0900

    [level 2] Title: H-Index, Time: 0.41 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,14 +1,23 @@
+/*
+1. 오름차순 정렬
+2. 중간값 찾음 
+3. 중간값 기준 오른쪽은 h편 이상 왼쪽은 h편 이하
+4. -- 순회 시작
+5. citations.length - i 가 h 이상이면 해당하는 h를 반환 
+*/
+
+11-5
+[0,1,2,3,4,5,6,7,8,9,10];
+
 function solution(citations) {
-    // 내림차순으로 정렬
-    const sortedCitations = citations.sort((a, b) => b - a);
+    citations.sort((a,b) => a-b);
+    const n = citations.length;
     
-    // H-Index 계산
-    for (let h = 0; h < sortedCitations.length; h++) {
-        if (sortedCitations[h] < h + 1) {
-            return h;
-        }
-    }
+    for(let i = parseInt(citations.length/2); i >= 0; i--) {
+        console.log(i);
+        const h = citations[i];
+        if(n-i >= h) return h;
+    };
     
-    // 모든 논문이 h-index 조건을 만족하는 경우
-    return sortedCitations.length;
+    return answer;
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?itm_content=course14743) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.67 ms
+메모리: 33.4 MB, 시간: 0.41 ms
 
 ### 구분
 
@@ -12,11 +12,11 @@
 
 ### 채점결과
 
-정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 제출 일자
 
-2024년 12월 24일 19:03:19
+2024년 12월 24일 19:03:56
 
 ### 문제 설명
 
