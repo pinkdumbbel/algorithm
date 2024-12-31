34b7de4c486b56f99d25ea7617b2dd58aa1c8586
pinkdumbbel <tlacjd665@gmail.com>
2024-12-31 20:51:41 +0900

    [level 2] Title: 튜플, Time: 58.80 ms, Memory: 41.4 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=javascript) 
 
 ### 성능 요약
 
-메모리: 54.5 MB, 시간: 74.61 ms
+메모리: 41.4 MB, 시간: 58.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 31일 20:24:20
+2024년 12월 31일 20:51:39
 
 ### 문제 설명
 
--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,50 +1,8 @@
-/*
-문자열을 순회 하면서 각 조합을 배열로 만든다.
-만들어진 배열을 순회 한다.
-배열의 길이로 오름차순 정렬한다.
-배열의 원소에 있는 튜플을 오름차순 정렬한다.
-
-조합의 길이만큼 돌면서 
-각 튜플을 순회한다. 
-그러면 하나씩 answer에 push하면 될듯
-*/
-
 function solution(s) {
-    const answer = [];
-    let combi = [];
-    let tmp = [];
-    
-    s = s.substring(1, s.length-1);
-    
-    for(const x of s) {
-        if(x === '{') continue;
-        
-        if(x === '}') {
-            combi.push(tmp.join('').split(',').filter((s) => !!s));
-            tmp = [];
-        } else {
-            tmp.push(x);
-        }
-    };
-    
-    combi = combi.sort((a,b) => a.length - b.length)
-    const set = new Set();
-    
-    for(let i = 0; i < combi.length; i++) {
-        for(let j = 0; j < combi[i].length; j++) {
-            set.add(Number(combi[i][j]));    
-        };
-    };
-    
-    
-    
-    return [...set];
+    return s
+            .slice(1,-1)
+            .split('},{')
+            .map((s) => s.replace(/[{}]/g, '').split(',').map(Number))
+            .sort((a,b) => a.length - b.length)
+            .reduce((a,b) => [...a, ...b.filter((el) => !a.includes(el))], []);
 }
-
-// 중복된 원소 존재 가능
-// 튜플의 순서가 같아야 됨 
-// 원소의 개수는 정해져 있음
-// 튜플을 집합으로 표현한 s가 입력으로 주어짐
-// 집합은 순서가 달라도 상관 없음 
-// 이때 집합이 표현하는 튜플을 구하라
-
