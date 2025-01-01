e57f9ea3856694b4b80743ebd3a70c066561ec80
pinkdumbbel <tlacjd665@gmail.com>
2025-01-01 18:38:06 +0900

    [level 2] Title: 프로세스, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,26 @@
+//[1, 1, 9, 1, 1, 1]	
+//우선순위별 나열하기위해 내림차순 정렬
+//포인터만 움직이면서 우선순위 비교 
+function solution(priorities, location) {
+    let answer = 0;
+    
+    const sorted = [...priorities].sort((a,b) => b-a);
+    let pt = mpt = 0;
+    
+    while(mpt < priorities.length) {
+        const p = priorities[pt];
+        
+        if(p === sorted[mpt]) { 
+            answer++;
+            mpt++;
+            
+            if(pt === location) return answer;
+        }
+        
+        pt = pt == priorities.length-1 ? 0 : pt+1;
+        
+    };
+    
+    return answer;
+}
+
