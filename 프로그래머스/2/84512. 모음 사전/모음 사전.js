50a8010e5716a9453b5a423d25ef107fc18909d2
pinkdumbbel <tlacjd665@gmail.com>
2025-01-27 18:10:17 +0900

    [level 2] Title: 모음 사전, Time: 25.40 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -2,7 +2,6 @@ function solution(word) {
     var answer = 0;
     const words = ['A', 'E', 'I', 'O', 'U']; 
     const n = words.length;
-    const arr = [];
     let flag = false;
     
     function dfs(s = '') {
