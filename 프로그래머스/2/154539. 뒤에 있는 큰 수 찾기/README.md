b214df0d52f3743e9ea5c9448dc4ee2b21775913
pinkdumbbel <tlacjd665@gmail.com>
2025-01-30 13:30:05 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 46.63 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/README.md
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 뒤에 있는 큰 수 찾기 - 154539 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154539) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154539?language=javascript) 
 
 ### 성능 요약
 
-메모리: 160 MB, 시간: 46.92 ms
+메모리: 160 MB, 시간: 46.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 28일 20:41:53
+2025년 01월 30일 13:30:03
 
 ### 문제 설명
 
--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,14 +1,16 @@
 function solution(numbers) {
     const n = numbers.length;
-    const answer = new Array(n).fill(-1);
+    const result = new Array(n).fill(-1);
     const stack = [];
     
     for (let i = 0; i < n; i++) {
-        while (stack.length > 0 && numbers[stack[stack.length - 1]] < numbers[i]) {
-            answer[stack.pop()] = numbers[i];
+        const n = numbers[i];
+        
+        while (stack.length > 0 && numbers[stack[stack.length - 1]] < n) {
+            result[stack.pop()] = n;
         }
         stack.push(i);
     }
     
-    return answer;
+    return result;
 }
