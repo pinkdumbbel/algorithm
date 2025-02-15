f34da382c4dd4911082006144069a2fe627d3d85
pinkdumbbel <tlacjd665@gmail.com>
2025-02-15 16:09:24 +0900

    [level 2] Title: 택배상자, Time: 32.00 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.js
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -1,35 +1,17 @@
 function solution(order) {
-    let answer = 0;
+    let stack = [];
+    let index = 0;
+    let count = 0;
     
-    order.reverse();
-    const n = order.length;
-    const box = Array.from({length: n}, (_, i) => n-i);
-    const stack = [];
-    
-    let o = order.pop();
-    
-    while(box.length) {
-        const b = box.pop();
+    for (let box = 1; box <= order.length; box++) {
+        stack.push(box);
         
-        if(b === o) {
-            answer++;
-            o = order.pop();
-        } else {
-            while(stack[stack.length-1] === o) {
-                answer++
-                stack.pop();
-                o = order.pop();
-            };
-            
-            stack.push(b);
-        };
-    };
+        while (stack.length && stack[stack.length - 1] === order[index]) {
+            stack.pop();
+            index++;
+            count++;
+        }
+    }
     
-    while(stack.length && stack[stack.length-1] === o) {
-        answer++
-        stack.pop();
-        o = order.pop();
-    };
-    
-    return answer;
+    return count;
 }
