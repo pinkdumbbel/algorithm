2e34eeb1abf3b03b7e343e00be646f641d8613e0
pinkdumbbel <tlacjd665@gmail.com>
2025-03-29 20:19:36 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,16 +1,6 @@
 function solution(s) {
-    let answer = '';
-    let isUpperCase = false;
-    
-    for(const x of s.toLowerCase()) {
-        if(x === ' ') {
-            answer += x;
-            isUpperCase = false;
-        } else {
-            answer+=(isUpperCase ? x : x.toUpperCase());
-            isUpperCase = true;
-        };
-    };
-    
-    return answer;
+    return s
+            .split(' ')
+            .map((s) => s.slice(0,1).toUpperCase()+s.slice(1).toLowerCase())
+            .join(' ');
 }
