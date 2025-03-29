ab71f32d4b78fbdae31a639e4c7beb72c51667eb
pinkdumbbel <tlacjd665@gmail.com>
2025-03-29 20:10:56 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.76 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,7 +1,6 @@
 function solution(A,B){
-    var answer = 0;
     A.sort((a,b) => a-b);
     B.sort((a,b) => b-a);
-       
-    return A.reduce((a,b,i) => a + (b*B[i]), 0);
+    
+    return A.reduce((a,b,i) => a + (b * B[i]), 0);
 }
