c4a0b223010f8e1c3211fbc598d9bd1eb42df5a4
pinkdumbbel <tlacjd665@gmail.com>
2025-04-06 18:24:14 +0900

    [level 2] Title: 귤 고르기, Time: 30.62 ms, Memory: 48.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
 
 ### 성능 요약
 
-메모리: 44.2 MB, 시간: 27.14 ms
+메모리: 48.3 MB, 시간: 30.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 07일 19:22:59
+2025년 04월 06일 18:24:13
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,16 +1,13 @@
 function solution(k, tangerine) {
+    let answer = 0;
     const map = new Map();
 
     tangerine.forEach((t) => {
-        map.set(t, (map.get(t) ?? 0) + 1);
+        map.set(t, map.get(t) ? map.get(t)+1 : 1);
     });
     
-    const sortedValues = [...map.values()].sort((a,b) => b-a);
-    
     let sum = 0;
-    let answer = 0;
-    
-    for(const v of sortedValues) {
+    for(const v of [...map.values()].sort((a,b) => b-a)) {
         sum += v;
         answer++;
         if(sum >= k) return answer;
