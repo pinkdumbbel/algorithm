fa342cfb1923c23c92c0750396a3c865aca0cbee
pinkdumbbel <tlacjd665@gmail.com>
2025-04-07 15:38:23 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.42 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,13 +1,12 @@
 function solution(n) {
-    if (n <= 2) return n;
-    
-    let dp = new Array(n + 1).fill(0);
+    const dp = new Array(n+1).fill(0);
     dp[1] = 1;
     dp[2] = 2;
     
-    for (let i = 3; i <= n; i++) {
-        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
-    }
+    for(let i = 3; i <= n; i++) {
+        dp[i] = (dp[i-1]+dp[i-2])%1234567
+    };
     
     return dp[n];
 }
+
