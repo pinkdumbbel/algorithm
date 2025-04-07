912a9cf4aa5baea584fd6cf66c48bfc6a357d903
pinkdumbbel <tlacjd665@gmail.com>
2025-04-07 16:37:26 +0900

    [level 2] Title: N개의 최소공배수, Time: 10.30 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -4,19 +4,22 @@ function solution(arr) {
     let n = 2;
     
     while(true) {
-      let isMinimumCommonMultiple = true;
-      answer = max * n;
+        let flag = true;
+        answer = max*n;
         
-      arr.forEach((x) => {
-        if(x === max) return;
-        if(!isMinimumCommonMultiple) return;
-          
-        if(answer%x !== 0) isMinimumCommonMultiple = false;
-      });
+        arr.forEach((el) => {
+            if(!flag) return;
+            if(answer%el !== 0) flag = false;
+        });
         
-      if(isMinimumCommonMultiple) return answer;
-      n++;
+        if(flag) return answer; 
+        n++;
     };
     
     return answer;
 }
+
+/*
+배열의 모든 값을 곱 누적하여 answer에 할당
+2중 반복으로 하나씩 나눈 후 그 값으로 모든 요소가 나누어 졌을때 그 값중 가장 작은값을 넣는다.
+*/
