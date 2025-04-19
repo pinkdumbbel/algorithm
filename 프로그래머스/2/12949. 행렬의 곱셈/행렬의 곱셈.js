4c4c56ca6af08f1c9564d45ab417cb41af2f2a93
pinkdumbbel <tlacjd665@gmail.com>
2025-04-19 17:28:46 +0900

    [level 2] Title: 행렬의 곱셈, Time: 15.17 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,17 +1,23 @@
 function solution(arr1, arr2) {
     return arr1.map((rows) => 
-                arr2[0].map((cols, y) => 
-                    rows.reduce((acc, row, x) => {
-                        return acc+(row*arr2[x][y])
-                    },0)));
-};
-
+                arr2[0].map((_,row) => 
+                    rows.reduce((a,b,col) => a + (b*arr2[col][row]),0)))
+}
 
 /*
-[[2, 3, 2], [4, 2, 4], [3, 1, 4]],
+행의 각각 i번째는 
+i행 * 곱행렬의 i열
+
 [
-    [5, 4, 3], 
-    [2, 4, 1], 
-    [3, 1, 1]
+    [2, 3, 2], 
+    [4, 2, 4], 
+    [3, 1, 4]
+]	
+[
+    [5,4,3],
+    [2,4,1],
+    [3,1,1]
 ]
 */
+
+
