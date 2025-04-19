83f583296fefa6fa3c1a5ed8e633fdfd828778a0
pinkdumbbel <tlacjd665@gmail.com>
2025-04-19 19:07:23 +0900

    [level 2] Title: 의상, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,11 +1,26 @@
 function solution(clothes) {
-    var answer = 0;
-    const map = new Map();
+    let answer = 0;
+    const hash = {};
     
-    clothes.forEach(([a,b]) => {
-        map.set(b, map.get(b) ? map.get(b)+1 : 1);
+    clothes.forEach(([_, type]) => {
+        if(hash[type]) {
+            hash[type]++
+        } else {
+            hash[type] = 1;
+        }
     });
     
-    return [...map.values()].reduce((acc,cur) => acc*(cur+1),1) - 1;
-}
+    const values = Object.values(hash);
+    
+// [1,2,3,4];
+// 2개씩 하는경우
+// 2 + 3 + 4 + 6 + 8 + 12;
 
+// 3개씩 
+// 6 + 12 + 24
+
+// 4개씩
+// 24
+    
+    return Object.values(hash).reduce((a,b) => a+(a*b),1)-1;
+}
