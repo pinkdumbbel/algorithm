5018c9e0d247f0479bcf9be319247dcad40fe96a
pinkdumbbel <tlacjd665@gmail.com>
2025-04-26 19:23:56 +0900

    [level 2] Title: [1차] 캐시, Time: 34.51 ms, Memory: 47 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680?language=javascript) 
 
 ### 성능 요약
 
-메모리: 50.2 MB, 시간: 245.13 ms
+메모리: 47 MB, 시간: 34.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 26일 19:18:51
+2025년 04월 26일 19:23:54
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,24 +1,23 @@
 function solution(cacheSize, cities) {
-    let answer = 0;
-    cities = cities.map((city) => city.toLowerCase());
-    let map = new Map();
-    
-    cities.forEach((city) => {        
-        if(map.get(city)) {
-            map.delete(city);
-            answer += 1;
-        } else {
-            answer += 5;
-        };
-        
-        map.set(city, true);
-        
-        if(map.size > cacheSize) {
-                const iter = map[Symbol.iterator]();
-                iter.next();
-                map = new Map([...iter]);
-        };
-    });
-    
-    return answer;
+  let answer = 0;
+  cities = cities.map((city) => city.toLowerCase());
+  let map = new Map();
+
+  cities.forEach((city) => {
+    if (map.get(city)) {
+      map.delete(city);
+      answer += 1;
+    } else {
+      answer += 5;
+    }
+
+    map.set(city, true);
+
+    if (map.size > cacheSize) {
+      const oldKey = map.keys().next().value;
+      map.delete(oldKey);
+    }
+  });
+
+  return answer;
 }
