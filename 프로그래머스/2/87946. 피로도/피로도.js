a5bd951fe853c91fa980455fa64027ad54b8b9a9
pinkdumbbel <tlacjd665@gmail.com>
2025-04-26 18:42:28 +0900

    [level 2] Title: 피로도, Time: 8.50 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,21 +1,22 @@
 function solution(k, dungeons) {
-    let answer = 0;
-    const n = dungeons.length;
-    
-    function dfs(hp, visited = new Set(), cnt = 0) {
-        answer = Math.max(cnt, answer);
-        
-        for(let i = 0; i < n; i++) {
-            if(!visited.has(i) && hp >= dungeons[i][0]) {
-                visited.add(i);
-                dfs(hp-dungeons[i][1], visited, cnt+1);
-                visited.delete(i);
-            };
-        };    
-    }
-    
-    dfs(k);
-    
-    return answer;
-}
+  let answer = 0;
 
+  const n = dungeons.length;
+  const chk = Array(n).fill(false);
+
+  const recur = (hp, cnt) => {
+    answer = Math.max(cnt, answer);
+      
+    for (let i = 0; i < n; i++) {
+        if (!chk[i] && dungeons[i][0] <= hp) {
+          chk[i] = true;
+          recur(hp-dungeons[i][1], cnt+1);
+          chk[i] = false;
+        }
+      }
+  };
+
+  recur(k, 0);
+
+  return answer;
+}
