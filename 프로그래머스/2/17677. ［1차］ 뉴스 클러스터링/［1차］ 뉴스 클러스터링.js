f9d1be65ba9238c614fdc4c7474215dc715e5838
pinkdumbbel <tlacjd665@gmail.com>
2025-04-27 19:03:07 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.26 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,40 +1,65 @@
 function solution(str1, str2) {
-    const set1 = createSet(str1);
-    const set2 = createSet(str2);
+    const sliceStr = (str) => {
+        str = str.toLowerCase();
+        const arr = [];
+        
+        for(let i = 0; i < str.length; i++) {
+            const sliced = str.slice(i, i+2);
+            if(sliced.length!==2 || sliced.match(/[^a-z]/g)) continue;
+            arr.push(sliced);
+        };
+        
+        return arr;
+    };
     
-     const counter1 = set1.reduce((acc, val) => {
-        acc[val] = (acc[val] || 0) + 1;
-        return acc;
-    }, {});
-
-    const counter2 = set2.reduce((acc, val) => {
-        acc[val] = (acc[val] || 0) + 1;
-        return acc;
-    }, {});
-
+    const arr1 = sliceStr(str1);
+    const arr2 = sliceStr(str2);
     
-    const intersection = Object.keys(counter1).reduce((acc, key) => {
-        if (counter2[key]) {
-            acc += Math.min(counter1[key], counter2[key]);
-        }
-        return acc;
-    }, 0);
-
-    const union = Object.keys({...counter1, ...counter2}).reduce((acc, key) => {
-        acc += Math.max(counter1[key] || 0, counter2[key] || 0);
-        return acc;
-    }, 0);
-
+    const intersection = (() => {
+        let size = 0;
+        
+        const map1 = new Map();
+        const map2 = new Map();
+        
+        arr1.forEach((str) => {
+            map1.set(str, (map1.get(str)??0)+1);
+        });
+        arr2.forEach((str) => {
+            map2.set(str, (map2.get(str)??0)+1);
+        });
+        
+        map1.forEach((value, key) => {            
+            size += Math.min(value, map2.get(key) ?? 0);    
+        });
+        return size;
+    })();  
     
-    return union === 0 ? 65536 : Math.floor((intersection / union) * 65536);
-
+    const union = (() => {
+        let size = 0;
+        
+        const map1 = new Map();
+        const map2 = new Map();
+        
+        arr1.forEach((str) => {
+            map1.set(str, (map1.get(str)??0)+1);
+        });
+        arr2.forEach((str) => {
+            map2.set(str, (map2.get(str)??0)+1);
+        });
+        
+        map1.forEach((value, key) => {            
+            size += Math.max(value, map2.get(key) ?? 0);    
+            if(map2.has(key)) map2.delete(key);
+        });
+        
+        if(map2.size) {
+            size += [...map2.values()].reduce((a,b) => a+b,0)    
+        };
+        
+        return size;
+    })();  
+    
+    if(!intersection && !union) return 65536;
+    
+    return Math.floor((intersection/union)*65536);
 }
-
-const createSet = (str) => str.toLowerCase()
-            .split('')
-            .slice(0, -1)
-            .map((char, idx) => {
-                const pair = str.toLowerCase().slice(idx, idx + 2);
-                return /^[a-z]{2}$/.test(pair) ? pair : null;
-            })
-            .filter(Boolean);
