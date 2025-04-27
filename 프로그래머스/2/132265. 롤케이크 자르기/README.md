12e81f88b7e6df2eed43803b32a55a4e9585dc43
pinkdumbbel <tlacjd665@gmail.com>
2025-04-27 18:01:54 +0900

    [level 2] Title: 롤케이크 자르기, Time: 121.19 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/2/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265) 
 
 ### 성능 요약
 
-메모리: 71.3 MB, 시간: 162.27 ms
+메모리: 71.3 MB, 시간: 121.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 26일 21:33:29
+2025년 04월 27일 18:01:52
 
 ### 문제 설명
 
--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,24 +1,18 @@
 function solution(topping) {
     let answer = 0;
-    const right = new Map();
-    const left = new Set();
+    const map = new Map();
+    const set = new Set();
     
-    topping.forEach((e) => {
-        right.set(e, (right.get(e) ?? 0) + 1);    
+    topping.forEach((t) => {
+        map.set(t, (map.get(t)??0)+1);
     });
     
-    topping.forEach((e) => {
-        const cnt = right.get(e);
-        right.set(e, cnt-1);
-        left.add(e);
+    topping.forEach((t) => {
+        set.add(t);
+        map.set(t, map.get(t)-1);
+        if(map.get(t) === 0) map.delete(t);
         
-        if(right.get(e) === 0) {
-            right.delete(e);    
-        };
-        
-        if(left.size === right.size) {
-            answer++
-        }
+        if(set.size === map.size) answer++;
     });
     
     return answer;
