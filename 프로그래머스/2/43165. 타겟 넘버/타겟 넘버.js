f6fb47d2abe4225ac7809ab12fd10fa49cf9084f
pinkdumbbel <tlacjd665@gmail.com>
2025-05-02 16:28:30 +0900

    [level 2] Title: 타겟 넘버, Time: 16.32 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,13 +1,13 @@
 function solution(numbers, target) {
-    var answer = 0;
+    let answer = 0;
     const n = numbers.length;
     
-    function dfs(L, sum) {
+    const dfs = (L, sum) => {
         if(L >= n) {
-          return sum === target ? 1 : 0;  
+            return sum === target ? 1 : 0
         } else {
-            return dfs(L+1, sum+numbers[L]) + dfs(L+1, sum-numbers[L])
-        }
+            return dfs(L+1, sum+numbers[L]) + dfs(L+1, sum-numbers[L]); 
+        };
     };
     
     return dfs(0, 0);
