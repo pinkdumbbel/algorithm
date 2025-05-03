3d225dc4135b13678ce8c59497423f2a8e436f61
pinkdumbbel <tlacjd665@gmail.com>
2025-05-03 18:20:57 +0900

    [level 2] Title: [3차] n진수 게임, Time: 36.67 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/README.md
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] n진수 게임 - 17687 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17687) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17687?language=javascript) 
 
 ### 성능 요약
 
-메모리: 37.6 MB, 시간: 7.41 ms
+메모리: 37.7 MB, 시간: 36.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 03일 18:16:24
+2025년 05월 03일 18:20:55
 
 ### 문제 설명
 
--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -4,17 +4,12 @@ function solution(n, t, m, p) {
     let turn = 0;
     
     while(answer.length < t) {
-        const str = cnt.toString(n).toUpperCase();
+        const str = (cnt++).toString(n).toUpperCase();
         
         for(const s of str) {            
-            if(turn%m === p-1) {
-                answer += s;
-                if(answer.length === t) return answer;
-            };
-            turn++;
+            if((turn++)%m === p-1) answer += s;
+            if(answer.length === t) return answer;
         }
-        
-        cnt++;
     };
     
     return answer;
