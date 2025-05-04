42a2e45cedcad5dea11b995ab1f56cf4c8447baa
pinkdumbbel <tlacjd665@gmail.com>
2025-05-04 15:39:21 +0900

    [level 2] Title: 주식가격, Time: 36.33 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -2,19 +2,23 @@ function solution(prices) {
     const n = prices.length;
     const answer = new Array(n).fill(0);
     const stack = [];
-
-    for (let i = 0; i < n; i++) {
-        while (stack.length && prices[stack[stack.length - 1]] > prices[i]) {
-            const j = stack.pop();
-            answer[j] = i - j;
+    
+    for(let i = 0; i < n; i++) {
+        const current = prices[i];
+        
+        if(prices[stack[stack.length-1]] > current) {
+            while(stack.length && prices[stack[stack.length-1]] > current) {
+                const idx = stack.pop();
+                answer[idx]++;
+            }
+        } 
+        
+        for(const index of stack) {
+            answer[index]++;
         }
+        
         stack.push(i);
-    }
-
-    while (stack.length) {
-        const j = stack.pop();
-        answer[j] = n - 1 - j;
-    }
-
+    };
+    
     return answer;
 }
