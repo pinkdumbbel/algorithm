868d2f944d8e89a0333f7c87552dffcd285f31b5
pinkdumbbel <tlacjd665@gmail.com>
2025-05-04 16:38:58 +0900

    [level 2] Title: 땅따먹기, Time: 35.80 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/README.md
+++ 프로그래머스/2/12913. 땅따먹기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 땅따먹기 - 12913 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913) 
 
 ### 성능 요약
 
-메모리: 72.5 MB, 시간: 35.47 ms
+메모리: 71.8 MB, 시간: 35.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 01일 21:34:35
+2025년 05월 04일 16:38:57
 
 ### 문제 설명
 
--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -1,12 +1,8 @@
 function solution(land) {
-    var answer = 0;
-
-    return Math.max(...land.reduce((a, c) => {
-        return [
-            c[0] + Math.max(a[1], a[2], a[3]),  
-            c[1] + Math.max(a[0], a[2], a[3]),
-            c[2] + Math.max(a[0], a[1], a[3]),
-            c[3] + Math.max(a[0], a[1], a[2]),
-        ];
-    }, [0, 0, 0, 0]));
+    return Math.max(...land.reduce((a,b) => ([
+        b[0] + Math.max(a[1], a[2], a[3]),
+        b[1] + Math.max(a[0], a[2], a[3]),
+        b[2] + Math.max(a[0], a[1], a[3]),
+        b[3] + Math.max(a[0], a[1], a[2]),
+    ]), [0,0,0,0]))
 }
