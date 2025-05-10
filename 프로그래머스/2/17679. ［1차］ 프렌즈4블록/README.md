7032886196cfb9703fcba9ccbc6e833c1668d02d
pinkdumbbel <tlacjd665@gmail.com>
2025-05-10 19:04:54 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 22.89 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/README.md
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 프렌즈4블록 - 17679 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17679?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17679) 
 
 ### 성능 요약
 
-메모리: 37.9 MB, 시간: 30.48 ms
+메모리: 37.4 MB, 시간: 22.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 03일 17:27:50
+2025년 05월 10일 19:04:52
 
 ### 문제 설명
 
--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,51 +1,50 @@
 function solution(m, n, board) {
-    let answer = 0;
-    board = board.map((s) => s.split(''));
+  let answer = 0;
+  board = board.map((blocks) => blocks.split(""));
+  const set = new Set();
+  let i = 0;
     
-    while(true) {
-        const toRemoved = new Set();
-        
-        for(let i = 0; i < m-1; i++) {
-            for(let j = 0; j < n-1; j++){
-                const v = board[i][j];
-                if(
-                    v &&
-                    v === board[i][j+1] &&
-                    v === board[i+1][j] &&
-                    v === board[i+1][j+1]
-                ) {
-                    toRemoved.add(`${i}:${j}`);
-                    toRemoved.add(`${i}:${j+1}`);
-                    toRemoved.add(`${i+1}:${j}`);
-                    toRemoved.add(`${i+1}:${j+1}`);
-                }
-            }
-        };
-        
-        if(toRemoved.size === 0) break;
-        
-        answer += toRemoved.size;
-        toRemoved.forEach((v) => {
-            const [i, j] = v.split(':').map(Number);
-            board[i][j] = null;
-        });
-        
-        for(let i = m-1; i >=0; i--) {
-            if(board[i].every((v) => v)) continue;
-            
-            for(let j = 0; j < n; j++) {
-                for(let k = i-1; k >= 0; k--) {
-                    if(board[i][j]) break;
-                    
-                    if(board[k][j]) {
-                        board[i][j] = board[k][j];
-                        board[k][j] = null;
-                    };
-                };
-            };
-        }
-    };
-    
-    return answer;
-}
+  while (true) {
+    for (let i = 0; i < m - 1; i++) {
+      for (let j = 0; j < n - 1; j++) {
+        const block = board[i][j];
 
+        if (
+          block && 
+          board[i][j + 1] === block &&
+          board[i + 1][j] === block &&
+          board[i + 1][j + 1] === block
+        ) {
+          set.add(`${i},${j}`);
+          set.add(`${i},${j + 1}`);
+          set.add(`${i + 1},${j}`);
+          set.add(`${i + 1},${j + 1}`);
+        }
+      }
+    }
+      
+    if(set.size === 0) return answer;
+      
+    set.forEach((value) => {
+        const [i, j] = value.split(",");
+        board[i][j] = null;
+    });
+
+      answer += set.size;
+      set.clear();
+
+      for (let i = m - 1; i >= 0; i--) {
+        for (let j = 0; j < n; j++) {
+          if (!board[i][j]) {
+            for (let k = i - 1; k >= 0; k--) {
+              if (board[k][j]) {
+                board[i][j] = board[k][j];
+                board[k][j] = null;
+                break;
+              }
+            }
+          }
+        }
+      }
+  }
+}
