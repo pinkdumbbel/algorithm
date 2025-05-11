10fcd0b38557cb6b08a4a7928694e4179d343b22
pinkdumbbel <tlacjd665@gmail.com>
2025-05-11 16:58:12 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 316.21 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -1,9 +1,21 @@
 function solution(numbers) {
-    return numbers.map(n => {
-        if (n % 2 === 0) return n + 1;
-        let binary = '0' + n.toString(2);
-        const lastZero = binary.lastIndexOf('0');
-        binary = binary.substring(0, lastZero) + '10' + binary.substring(lastZero + 2);
-        return parseInt(binary, 2);
+    const answer = [];
+    
+    // n이 짝수인 경우에 +1 해준 값
+    // n이 홀수인 경우에 마지막 01을 바꿔준 값
+    numbers.forEach((n) => {
+        if(n%2) {
+            let bin = '0'+n.toString(2);
+            const idx = bin.lastIndexOf('01');
+            const arr = bin.split('');
+            arr[idx] = '1';
+            arr[idx+1] = '0';
+            const min = arr.join('');
+            answer.push(parseInt(min,2));
+        } else {
+            answer.push(n+1);    
+        }
     });
+    
+    return answer;
 }
