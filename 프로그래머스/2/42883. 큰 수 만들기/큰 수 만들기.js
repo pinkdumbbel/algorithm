c951073940bd3e3b394834f77c40d0f027fb8fe3
pinkdumbbel <tlacjd665@gmail.com>
2025-05-18 16:58:58 +0900

    [level 2] Title: 큰 수 만들기, Time: 43.14 ms, Memory: 49.6 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,19 +1,23 @@
 function solution(number, k) {
-    const arr = number.split('').map(Number);
-    const n = arr.length;
-    
+    const n = number.length;
     const stack = [];
+    let removed = 0;
     
-    for(const num of arr){
-        while(stack.length && num > stack[stack.length-1] && k>0) {
-            stack.pop();   
-            k--;
-        };
+    for(let i = 0; i < n; i++) {
+        const v = Number(number[i]);        
+            
+        while(stack[stack.length-1] < v && removed < k) {
+            stack.pop();
+            removed++;
+        }  
         
-        stack.push(num);        
+        stack.push(v);
     };
     
-    if(stack.length === n) stack.pop();
+    while(removed < k) {
+        stack.pop();
+        removed++;
+    };
     
     return stack.join('');
 }
