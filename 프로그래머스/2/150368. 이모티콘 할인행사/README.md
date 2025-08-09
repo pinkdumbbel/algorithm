e0acccb95ad13d41de177bfa1a7dfc4e863eeade
pinkdumbbel <tlacjd665@gmail.com>
2025-08-09 20:08:31 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 114.15 ms, Memory: 45.6 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/README.md
+++ 프로그래머스/2/150368. 이모티콘 할인행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이모티콘 할인행사 - 150368 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150368) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150368?language=javascript) 
 
 ### 성능 요약
 
-메모리: 38.2 MB, 시간: 65.44 ms
+메모리: 45.6 MB, 시간: 114.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 09일 19:42:54
+2025년 08월 09일 20:08:30
 
 ### 문제 설명
 
--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -1,47 +1,50 @@
 function solution(users, emoticons) {
     const ratios = [10,20,30,40];
-    let maxSubscribers = 0;
+    const len = emoticons.length;
+    let maxSubs = 0;
     let maxSales = 0;
     
+    const discounts = [];
+    
     const dfs = (L, arr) => {
-        if(L === emoticons.length) {
-            let subscribers = 0;
-            let sales = 0;
-            
-            for(const [minDiscountRate, maxPrice] of users) {
-                let sum = 0;
-                
-                for(let i = 0; i < emoticons.length; i++) {    
-                    const ratio = arr[i];
-                    const emoticon = emoticons[i];
-                    
-                    if(minDiscountRate <= ratio) {
-                        const discountedPrice = emoticon-(emoticon*(ratio/100));
-                        sum += discountedPrice;
-                    }
-                };
-                
-                if(sum >= maxPrice) subscribers++;
-                else sales += sum;
-            };
-            
-            if(
-               (subscribers > maxSubscribers) || 
-               (subscribers === maxSubscribers && sales > maxSales)
-              ) {
-                maxSubscribers = subscribers;
-                maxSales = sales;
-            };
-                
+        if(L === len) {
+            discounts.push(arr);
             return;
-        };
+        }
         
         for(let i = 0; i < ratios.length; i++) {
-            dfs(L+1, [...arr, ratios[i]]);
+            dfs(L+1, [...arr, ratios[i]])
         }
     };
     
     dfs(0, []);
     
-    return [maxSubscribers, maxSales];
+    for(const discountRates of discounts) {
+        let subs = 0;
+        let sales = 0;
+        
+        for(const [minRate, maxPrice] of users) {
+            let sum = 0;
+            
+            for(let i = 0; i < len; i++) {
+                const ePrice = emoticons[i];
+                const discount = discountRates[i];
+                
+                if(discount >= minRate) {
+                    const disPrice = ePrice-(ePrice*(discount/100));
+                    sum+=disPrice;
+                }
+            };
+            
+            if(sum >= maxPrice) subs++;
+            else sales += sum;
+        };
+        
+        if(subs > maxSubs || (subs === maxSubs && sales > maxSales)){
+            maxSubs = subs;
+            maxSales = sales;
+        }
+    };
+    
+    return [maxSubs, maxSales];
 }
