49a3072a018dfc1a4f3a3a05b123c1f72953ca0b
pinkdumbbel <tlacjd665@gmail.com>
2025-10-09 19:52:35 +0900

    fix: 정렬 알고리즘 설명 추가

--- 자료구조/sort/mergeSort.mjs
+++ 자료구조/sort/mergeSort.mjs
@@ -1,9 +1,19 @@
+/**
+ * MergeSort
+ * 배열의 정렬되지 않은 원소들을 절반씩 쪼개서 정렬 후 합치는 알고리즘이다.
+ * 기본적으로 재귀로 구현되며 더 이상 쪼갤 수 없는 경우(원소가 하나 남을 때)까지 배열을 쪼갠 후
+ * 임시배열을 만들어 정렬을 한 이후에 원본배열에 덮어쓰는 방식으로 구현된다.
+ * 정렬을 하는 로직은 중간 지점을 기준으로 왼쪽 포인터와 오른쪽 포인터를 두고
+ * 서로 값을 비교하며 작은 값을 임시 배열에 저장한다.
+ */
+
 const mergeSort = (arr, start, end) => {
   if (start < end) {
-    let mid = parseInt((start + end) / 2);
+    const mid = parseInt((start + end) / 2);
 
     mergeSort(arr, start, mid);
     mergeSort(arr, mid + 1, end);
+
     merge(arr, start, mid, end);
   }
 };
@@ -25,13 +35,8 @@ const merge = (arr, start, mid, end) => {
     tp++;
   }
 
-  while (lp <= mid) {
-    tmpArr[tp++] = arr[lp++];
-  }
-
-  while (rp <= end) {
-    tmpArr[tp++] = arr[rp++];
-  }
+  while (lp <= mid) tmpArr[tp++] = arr[lp++];
+  while (rp <= end) tmpArr[tp++] = arr[rp++];
 
   for (let i = start; i <= end; i++) {
     arr[i] = tmpArr[i];
--- 자료구조/sort/quickSort.mjs
+++ 자료구조/sort/quickSort.mjs
@@ -1,28 +1,41 @@
-const quickSort = (arr, start, end) => {
-  if (start < end) {
-    let pivot = divide(arr, start, end);
+/**
+ * QuickSort
+ * 정렬되지 않은 원소 중 특정 지점의 하나의 값을 기준값(pivot)으로 설정한다.
+ * 기준값을 기준으로 기준값보다 작은 값들은 왼쪽, 큰 값들은 오른쪽 파티션으로 나누어 정렬한다.
+ * 각 파티션에 대해 동일한 방식으로 기준값을 지정하여 파티션을 나눈 후 정렬하는 로직으로 재귀적으로 동작한다.
+ * 파티션 과정에서는 왼쪽 포인터(파티션의 첫 번째 인덱스)와 오른쪽 포인터(파티션의 마지막 인덱스)를 설정하고,
+ * 왼쪽 포인터는 피벗보다 큰 값을, 오른쪽 포인터는 피벗보다 작은 값을 찾아 두 값을 스왑한다.
+ */
 
+const quickSort = (arr, start, end) => {
+  const pivot = divide(arr, start, end);
+
+  if (start < pivot - 1) {
     quickSort(arr, start, pivot - 1);
-    quickSort(arr, pivot + 1, end);
+  }
+
+  if (end > pivot) {
+    quickSort(arr, pivot, end);
   }
 };
 
-const divide = (arr, start, end) => {
-  const pivot = arr[start];
-  let lp = start + 1;
-  let rp = end;
+const divide = (arr, lp, rp) => {
+  const pivotValue = arr[parseInt((lp + rp) / 2)];
 
   while (lp <= rp) {
-    while (lp <= end && pivot >= arr[lp]) lp++;
+    //왼쪽 포인터를 하나씩 증가 하면서 피벗 값 보다 큰 수를 찾기
+    while (arr[lp] < pivotValue) lp++;
+    //오른쪽 포인터를 하나씩 감소 하면서 피벗 값 보다 작은 수 찾기
+    while (arr[rp] > pivotValue) rp--;
 
-    while (rp >= start + 1 && pivot <= arr[rp]) rp--;
-
-    if (lp <= rp) swap(arr, lp, rp);
+    if (lp <= rp) {
+      swap(arr, lp, rp);
+      lp++;
+      rp--;
+    }
   }
 
-  swap(arr, start, rp);
-
-  return rp;
+  return lp;
 };
 
 const swap = (arr, i, j) => {
@@ -30,6 +43,8 @@ const swap = (arr, i, j) => {
   arr[i] = arr[j];
   arr[j] = tmp;
 };
+//[3, 5, 2, 4, 1, 7, 8, 6];
+//[3, 1, 2, 4, 5, 7, 8, 6];
 
 const arr = [3, 5, 2, 4, 1, 7, 8, 6];
 
